1c0d6d12ee636f5cba73b87eb81ff8d78b61eecf
Mike Hellrich <bleemus@bleemus.com>
2022-05-29 16:10:21 +0000

    docker compose

--- new_minecraft.sh
+++ new_minecraft.sh
@@ -2,7 +2,8 @@
 
 DATE=`date +"%m%d%y"`
 
-sudo docker stop minecraft && sudo docker rm minecraft
+sudo docker compose down
+#sudo docker rm minecraft
 sudo docker image tag minecraft:latest minecraft:$DATE
-sudo docker build . --no-cache -t minecraft:latest
-sudo docker run -d --network=host --restart=unless-stopped --name minecraft -e EULA=TRUE -v /home/bleemus/mcdata/server.properties:/bedrock-server/server.properties -v /home/bleemus/mcdata/worlds/:/bedrock-server/worlds minecraft:latest
+sudo docker compose build . --no-cache
+sudo docker compose up -d
