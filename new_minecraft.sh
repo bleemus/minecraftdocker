a022bcdc2791e6a7ae7deadfae5223d376191cec
Mike Hellrich <bleemus@bleemus.com>
2022-06-21 16:47:25 +0000

    mods

--- new_minecraft.sh
+++ new_minecraft.sh
@@ -6,4 +6,5 @@ sudo docker compose down
 #sudo docker rm minecraft
 sudo docker image tag minecraft:latest minecraft:$DATE
 sudo docker compose build . --no-cache
+sudo docker build . --no-cache -t minecraft:latest
 sudo docker compose up -d
